4653fb0b37b294f9ec0b50501bac0c0c06ec0177
imnotmoon <imnotmoon@gmail.com>
2020-10-22 16:58:32 +0900

    2143 정답
    collections.Counter를 사용해서 시간 단축

--- baekjoon/2143.py
+++ baekjoon/2143.py
@@ -1,3 +1,5 @@
+from collections import Counter
+
 # INPUT
 T = int(input())
 n = int(input())
@@ -5,3 +7,27 @@ A = list(map(int, input().split()))
 m = int(input())
 B = list(map(int, input().split()))
 
+#  부분합 구함
+aSum = []
+bSum = []
+for i in range(n):
+    tmp = A[i]
+    aSum.append(tmp)
+    for j in range(i+1, n):
+        tmp += A[j]
+        aSum.append(tmp)
+
+for i in range(m):
+    tmp = B[i]
+    bSum.append(tmp)
+    for j in range(i+1, m):
+        tmp += B[j]
+        bSum.append(tmp)
+
+total = 0
+c = Counter(bSum)
+count = 0
+for i in aSum :
+    count += c[T-i]
+
+print(count)
