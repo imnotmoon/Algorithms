1d0717746e70f9a53de253a1ca4fe065e6ed9213
imnotmoon <imnotmoon@gmail.com>
2020-11-25 00:21:17 +0900

    Update 2468.py
    visited 체크하는 부분 위치 틀려서 수정 -> 정답

--- baekjoon/2468.py
+++ baekjoon/2468.py
@@ -1,5 +1,6 @@
 # 백준 2468
-
+import sys
+sys.setrecursionlimit(100000)
 def dfs(height, startY, startX):
     # 방문한 인덱스 튜플들의 리스트
     queue = list()
@@ -7,20 +8,20 @@ def dfs(height, startY, startX):
     while(queue):
         current = queue[0]
         queue.pop(0)
-        visited[current[0]][current[1]] = 1
         for i in range(4) :
             x = current[1]+dx[i]
             y = current[0]+dy[i]
             if (0 <= x < N) and (0 <= y < N) and visited[y][x] == 0 :
                 if area[y][x] > height :
+                    visited[y][x] = 1
                     queue.append((y, x))
 
 # 1. 입력
-N = int(input())
 dx = [1, 0, -1, 0]
 dy = [0, 1, 0, -1]
-
-area = [list(map(int, input().split())) for _ in range(N)]
+r = sys.stdin.readline
+N = int(r())
+area = [list(map(int, r().split(' '))) for _ in range(N)]
 maxHeight = max(map(max, area))
 
 # 2. 높이마다 섬 개수 비교
@@ -36,6 +37,6 @@ for i in range(0, maxHeight) :  # 높이마다
                 dfs(i, y, x)
                 count += 1
 
-    maxIslands = max(maxIslands, count)    
+    maxIslands = max(maxIslands, count)
 
 print(maxIslands)
