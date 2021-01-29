2da5d1093797b2eaf5130a4548ebeef2526f1f27
imnotmoon <imnotmoon@gmail.com>
2021-01-30 03:03:47 +0900

    1520 : 재귀dfs + dp, pypy는 반복문에 빠른 대신 메모리를 많이 먹음

--- baekjoon/1520.py
+++ baekjoon/1520.py
@@ -1,54 +1,24 @@
-# 1년 전 시간초과 났던거
-
-몇번째 순서에 갔는지 등등을 visisted로 저장할 수 있음 <- dp의 memoization과 병행
-
-
+# baekjoon 1520
+from sys import stdin
+import sys
+sys.setrecursionlimit(10**6)
+input = stdin.readline
 
 m, n = tuple(map(int, input().split()))
-_map = [list(map(int, input().split())) for _ in range(m)]
+arr = [list(map(int, input().split())) for _ in range(m)]
+dp = [[-1 for _ in range(n)] for _ in range(m)]
+dy, dx = [1, 0, -1, 0], [0, 1, 0, -1]
 
-dx = [1, 0, -1, 0]
-dy = [0, 1, 0, -1]
+def dfs(y, x):
+    if (y, x) == (m-1, n-1):
+        return 1
+    if dp[y][x] != -1:
+        return dp[y][x]
+    dp[y][x] = 0        # visited
+    for i in range(4):
+        cy, cx = y+dy[i], x+dx[i]
+        if (0<=cy<m) and (0<=cx<n) and arr[cy][cx] < arr[y][x]:
+            dp[y][x] += dfs(cy,cx)
+    return dp[y][x]
 
-start = (0, 0)
-dp = [[0 for col in range(n)] for row in range(m)]
-dp[0][0] = 1
-
-for i in range(m):
-    for j in range(n) :
-        count = dp[i][j]
-        for k in range(4) :
-            if i+dy[k] >= 0 and i+dy[k] < m and j+dx[k] >= 0 and j+dx[k] < n :
-                if(_map[i+dy[k]][j+dx[k]] > _map[i][j]) :
-                    count += dp[i+dy[k]][j+dx[k]]
-        dp[i][j] = count
-for i in range(m):
-    for j in range(n) :
-        count = 0
-        for k in range(4) :
-            if i+dy[k] >= 0 and i+dy[k] < m and j+dx[k] >= 0 and j+dx[k] < n :
-                if(_map[i+dy[k]][j+dx[k]] > _map[i][j]) :
-                    count += dp[i+dy[k]][j+dx[k]]
-        dp[i][j] = max(dp[i][j], count)
-
-print(dp)
-
-print(dp[m-1][n-1])
-
-# while queue :
-#     # print(queue)
-#     now = queue.pop(0)
-#     # print(now)
-#     for i in range(4) :
-#         # 경계 내에 있을 경우
-#         if now[0]+dy[i] >= 0 and now[0]+dy[i] < m and now[1]+dx[i]>=0 and now[1]+dx[i] < n :
-#             # 지금 칸보다 작은 값일 경우
-#             if _map[now[0]][now[1]] > _map[now[0]+dy[i]][now[1]+dx[i]] :
-#                 # 큐에 추가
-#                 if now[0]+dy[i] == m-1 and now[1]+dx[i] == n-1 :
-#                     print(_map[m-1][n-1])
-#                     count += 1
-#                     continue
-#                 else :
-#                     queue.append((now[0]+dy[i], now[1]+dx[i]))
-# print(count)
+print(dfs(0,0))
