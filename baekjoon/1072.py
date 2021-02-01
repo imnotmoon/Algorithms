907f4d6aa1c6fdd78aad0de733b596a5237626a6
imnotmoon <imnotmoon@gmail.com>
2021-02-01 23:42:31 +0900

    1072 : binary search

--- baekjoon/1072.py
+++ baekjoon/1072.py
@@ -1,19 +1,16 @@
 x, y = tuple(map(int, input().split()))
 z = int(y*100/x)
 
-left = 0
-right = 1000000001
+left, right = 0, 1000000001
 mid = int((left+right)/2)
 ret = -1
 prev_mid = right
 
 while(left < right):
-    # print(mid)
     if prev_mid == mid :
         break
     new_z = int((y+mid)*100/(x+mid))
     before_new_z = int((y+mid-1)*100/(x+mid-1))
-    # print(str(z) + " =?= " + str(before_new_z) + " < " + str(new_z))
     if before_new_z == z and new_z > z:
         ret = mid
         break
