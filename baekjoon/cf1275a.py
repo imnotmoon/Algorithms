eaad9ddced5a15632011536dc6ac5b9fa97bdb5c
imnotmoon <imnotmoon@gmail.com>
2021-02-25 21:45:00 +0900

    코드포스 1275A : greedy

---
+++ baekjoon/cf1275a.py
@@ -0,0 +1,16 @@
+# codeforce 1275-A
+
+t = int(input())
+segs = []
+for i in range(t):
+    segs.append(int(input()))
+
+for seg in segs:
+    result = ''
+    while(seg > 0):
+        if seg == 3:
+            result += '7'
+            break
+        result += '1'
+        seg -= 2
+    print(result)
