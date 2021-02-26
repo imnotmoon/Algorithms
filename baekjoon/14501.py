7889162fc4927918ccd0ec326f22b30c98eea770
imnotmoon <imnotmoon@gmail.com>
2021-02-26 19:07:32 +0900

    **14501 : dp...

---
+++ baekjoon/14501.py
@@ -0,0 +1,25 @@
+# baekjoon 14510
+
+n = int(input())
+timetable = []
+
+for i in range(n):
+    timetable.append(list(map(int, input().split())))
+
+dp = list(map(lambda x: x[1], timetable))
+
+for i in range(n):
+    to = timetable[i][0]
+
+    # 퇴사한 다음 상담이 끝나는 스케줄의 경우
+    # 상담 접수가 불가능하므로 삭제
+    if i + to > n:
+        dp[i] = 0
+        continue
+
+    t = dp[i]
+    for j in range(i + to, n):
+        if t + timetable[j][1] > dp[j]:
+            dp[j] = t + timetable[j][1]
+print(dp)
+print(max(dp))
