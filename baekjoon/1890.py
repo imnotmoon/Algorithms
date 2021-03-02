b010442d4bf99a0d7ec71d5e0dec20e167c81a1f
imnotmoon <imnotmoon@gmail.com>
2021-03-03 02:59:32 +0900

    1890 : dp

--- baekjoon/1890.py
+++ baekjoon/1890.py
@@ -4,12 +4,14 @@ import sys
 input = sys.stdin.readline
 
 n = int(input())
-board = [list(map(int, input().split()))]
+board = [list(map(int, input().split())) for _ in range(n)]
 dp = [[0 for _ in range(n)] for _ in range(n)]
 dp[0][0] = 1
 
 for i in range(n):
     for j in range(n):
+        if i == n-1 and j == n-1:
+            break
         jump = board[i][j]
         if(i+jump < n):
             dp[i+jump][j] = dp[i+jump][j] + dp[i][j]
