e09835babaac30d78df8805875c83d40c54e3575
imnotmoon <imnotmoon@gmail.com>
2021-03-05 02:39:40 +0900

    2583 : bfs

--- baekjoon/2583.py
+++ baekjoon/2583.py
@@ -1,5 +1,6 @@
 # baekjoon 2583
 import sys
+from collections import deque
 input = sys.stdin.readline
 
 m, n, k = map(int, input().split())
@@ -9,10 +10,39 @@ for i in range(k):
 
 area = [[0 for _ in range(n)] for _ in range(m)]
 for r in rect:
-    fy, fx, ty, tx = r[0], r[1], r[2], r[3]
-    for i in range(fy, fy+ty):
-        for j in range(fx, fx+tx):
-            area[i][j] = 1
+    fx, fy, tx, ty = r[0], r[1], r[2], r[3]
+    for i in range(fy, ty):
+        for j in range(fx, tx):
+            area[i][j] = 2
 
-for i in area:
-    print(area)
+dy, dx = [0, 1, 0, -1], [1, 0, -1, 0]
+
+
+def bfs(y, x):
+    global area
+    count = 0
+    queue = deque()
+    queue.append((y, x))
+    area[y][x] = 1
+    while queue:
+        cy, cx = queue.popleft()
+        count += 1
+        for i in range(4):
+            yy, xx = cy+dy[i], cx+dx[i]
+            if (0 <= yy < m) and (0 <= xx < n) and area[yy][xx] == 0:
+                area[yy][xx] = 1
+                queue.append((yy, xx))
+    return count
+
+
+area_count = 0
+area_size = []
+for i in range(m):
+    for j in range(n):
+        if area[i][j] == 0:
+            area_size.append(bfs(i, j))
+            area_count += 1
+
+print(area_count)
+for i in sorted(area_size):
+    print(i, end=' ')
