e548f3185ca68689b5c2f0842743a342a4a583d3
imnotmoon <imnotmoon@gmail.com>
2021-03-09 05:04:26 +0900

    11053 : LIS

---
+++ baekjoon/11053.py
@@ -0,0 +1,12 @@
+# baekjoon 11053
+
+n = int(input())
+arr = list(map(int, input().split()))
+dp = [1 for _ in range(n)]
+for i in range(n):
+    for j in range(0, i):
+        if arr[j] < arr[i]:
+            dp[i] = max(dp[i], dp[j]+1)
+    print(dp)
+
+print(max(dp))
