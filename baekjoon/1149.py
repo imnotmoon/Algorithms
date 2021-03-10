f18091277e0ea3e9a9d25670e621bfc68a944e7f
imnotmoon <imnotmoon@gmail.com>
2021-03-10 11:19:33 +0900

    1149 : 색이 3개라 나머지 두 색만 탐색 가능

---
+++ baekjoon/1149.py
@@ -0,0 +1,21 @@
+# baekjoon 1149
+
+import sys
+input = sys.stdin.readline
+
+n = int(input())
+house = []
+for i in range(n):
+    house.append(list(map(int, input().split())))
+
+print(house)
+
+dp1, dp2, dp3 = [0 for _ in range(
+    n+1)], [0 for _ in range(n+1)], [0 for _ in range(n+1)]
+dp1[1], dp2[1], dp3[1] = house[0][0], house[0][1], house[0][2]
+for i in range(2, n+1):
+    dp1[i] = min(dp2[i-1]+house[i-1][0], dp3[i-1]+house[i-1][0])
+    dp2[i] = min(dp1[i-1]+house[i-1][1], dp3[i-1]+house[i-1][1])
+    dp3[i] = min(dp1[i-1]+house[i-1][2], dp2[i-1]+house[i-1][2])
+
+print(min(dp1[n], dp2[n], dp3[n]))
