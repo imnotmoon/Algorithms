4f836465461f8e9b0aa17f85d03f552b68035fc1
imnotmoon <imnotmoon@gmail.com>
2021-03-13 23:22:31 +0900

    **2156 : dp..

--- baekjoon/2156.py
+++ baekjoon/2156.py
@@ -1,25 +1,17 @@
-# baekjoon 2156 *****
-n = int(input())
-wines = []
-for i in range(n):
-    wines.append(int(input()))
+# baekjoon 2156
+# 태형이가 도와줌
 
-dp = [0]
-dp.append(wines[0])
-if n > 2:
-    dp.append(wines[0]+wines[1])
+n = int(input())
+wine = [int(input()) for _ in range(n)]
+dp = [0, wine[0]]
+
+if n > 1:
+    dp.append(wine[0]+wine[1])
 
 for i in range(3, n+1):
-    case1 = wines[i-1]+dp[i-2]
-    case2 = wines[i-1]+wines[i-2]+dp[i-3]
-    case3 = dp[i-1]
-    # print(wines, wines[i-1])
-    # print("이번 포도주를 먹고 이전 포도주를 먹지 않은 경우 : ", case1)
-    # print("이번 포도주를 먹고 이전 포도주도 먹은 경우 : ", case2)
-    # print("이번 포도주를 먹지 말아야 하는 경우 : ", case3)
-
-    dp.append(max(case1, case2, case3))
-    # print(dp)
-    # print()
+    # wine은 i-1이 오늘
+    # dp는 i가 오늘
+    # 그저께까지+오늘(어제 안먹음), 그저께까지+어제+오늘, 어제까지+오늘안먹음
+    dp.append(max(dp[i-2]+wine[i-1], wine[i-1]+wine[i-2]+dp[i-3], dp[i-1]+0))
 
 print(dp[n])
