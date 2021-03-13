123297b82538d6d8e61e43f3fdb774d63235a193
imnotmoon <imnotmoon@gmail.com>
2021-03-14 00:54:51 +0900

    **12865 : dp..

--- baekjoon/12865.py
+++ baekjoon/12865.py
@@ -1,1 +1,21 @@
 # baekjoon 12865
+n, k = map(int, input().split())
+weight, value = [], []
+for i in range(n):
+    w, v = map(int, input().split())
+    weight.append(w)
+    value.append(v)
+
+dp = [[0 for _ in range(k+1)] for _ in range(n)]
+
+for i in range(n):
+    for j in range(1, k+1):
+        if j-weight[i] >= 0:
+            dp[i][j] = max(dp[i-1][j], dp[i-1][j-weight[i]]+value[i])
+        else:
+            dp[i][j] = dp[i-1][j]
+    # for j in dp:
+    #     print(j)
+    # print()
+
+print(dp[n-1][k])
