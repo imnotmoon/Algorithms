8d8f984ff1d2f0274f2dcfb4b6b7cd98db4a00da
imnotmoon <imnotmoon@gmail.com>
2021-03-23 14:46:21 +0900

    14916 : dp

--- baekjoon/1309.py
+++ baekjoon/1309.py
@@ -2,10 +2,8 @@
 
 n = int(input())
 dp = [0, 3, 7]
-if n == 1:
-    print(3)
-elif n == 2:
-    print(7)
+if n < 3:
+    print(dp[n])
 else:
     for i in range(3, n+1):
         dp.append((dp[i-2]+2*dp[i-1]) % 9901)
