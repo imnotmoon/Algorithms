99414ca8b9373cfc0aeb83ae6c458b7faad8c31e
imnotmoon <imnotmoon@gmail.com>
2021-10-24 19:49:50 +0900

    1991 : tree order

---
+++ baekjoon/1991.py
@@ -0,0 +1,40 @@
+# baekjoon 1991
+import sys
+import math
+input = sys.stdin.readline
+n, t = int(input()), dict()
+result = ''
+for i in range(n):
+  root, left, right = map(str, input().split())
+  t[root] = (left, right)
+
+def preorder(root):
+  global result
+  result += root
+  left, right = t[root]
+  if left != '.' : preorder(left)
+  if right != '.' : preorder(right)
+
+
+def inorder(root):
+  global result
+  left, right = t[root]
+  if left != '.': inorder(left)
+  result += root
+  if right != '.': inorder(right)
+
+def postorder(root):
+  global result
+  left, right = t[root]
+  if left != '.': postorder(left)
+  if right != '.' : postorder(right)
+  result += root
+
+preorder('A')
+print(result)
+result = ''
+inorder('A')
+print(result)
+result = ''
+postorder('A')
+print(result)
