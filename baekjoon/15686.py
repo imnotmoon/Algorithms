2cd4e66e5f31aa01be7e7fa851f6e3604568f3d5
imnotmoon <imnotmoon@gmail.com>
2021-11-03 03:18:58 +0900

    *15686: brute force

---
+++ baekjoon/15686.py
@@ -0,0 +1,25 @@
+# baekjoon 15686
+
+import sys
+input = sys.stdin.readline
+from itertools import combinations
+
+N, M = map(int, input().split())
+city, chickens, homes = [ list(map(int, input().split())) for _ in range(N) ], [], []
+
+for i in range(N): 
+  for j in range(N) :
+    if city[i][j] == 2: chickens.append((i, j))
+    if city[i][j] == 1: homes.append((i, j))
+
+to_close = list(combinations(chickens, M))
+result = [0] * len(to_close)
+
+for i in homes:
+  for j in range(len(to_close)):
+    a = N+N
+    for k in to_close[j]:
+      a = min(abs(i[0]-k[0]) + abs(i[1]-k[1]), a)  # 각 집에 대해 치킨집들을 순회 -> 가장 가까운 치킨집 구함
+    result[j] += a
+
+print(min(result))
