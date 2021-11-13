42077b46ab80fc6cd0fc57df64eb0d4bfd533905
imnotmoon <imnotmoon@gmail.com>
2021-11-13 16:22:17 +0900

    숏코딩 랭킹먹을라고 고침

--- baekjoon/16973.py
+++ baekjoon/16973.py
@@ -1,36 +1,29 @@
-# beakjoon 16973
 import sys
 from collections import deque
 input = sys.stdin.readline
-
 N, M = map(int, input().split())
 board = [ list(map(int, input().split())) for _ in range(N) ]
 H, W, Sr, Sc, Fr, Fc = map(int, input().split())
-dy, dx = [1, 0, -1, 0], [0, 1, 0, -1]
-walls = []
-
+dy, dx, walls = [1, 0, -1, 0], [0, 1, 0, -1], []
 for i in range(N):
   for j in range(M):
     if board[i][j] == 1: walls.append((i, j))
-
-def check_available(y, x):
+def ca(y, x):
   for Wy, Wx in walls:
-    if y <= Wy < y+H and x <= Wx < x+W : 
-      return False
+    if y <= Wy < y+H and x <= Wx < x+W : return False
   return True
-
-visited = [ [0 for _ in range(M)] for _ in range(N) ]
-visited[Sr-1][Sc-1] = 1
-queue = deque([(Sr-1, Sc-1, 0)])
-while queue:
-  y, x, cnt = queue.popleft()
+V = [ [0 for _ in range(M)] for _ in range(N) ]
+V[Sr-1][Sc-1] = 1
+Q = deque([(Sr-1, Sc-1, 0)])
+while Q:
+  y, x, cnt = Q.popleft()
   if y == Fr-1 and x == Fc-1: 
     print(cnt)
     exit()
   for i in range(4):
     yy, xx = y+dy[i], x+dx[i]
-    if 0 <= yy < N and 0 <= xx < M and 0 <= yy+H-1 < N and 0 <= xx+W-1 < M and visited[yy][xx] == 0:
-      if check_available(yy, xx): 
-        queue.append((yy, xx, cnt+1))
-        visited[yy][xx] = 1
+    if 0 <= yy < N and 0 <= xx < M and 0 <= yy+H-1 < N and 0 <= xx+W-1 < M and V[yy][xx] == 0:
+      if ca(yy, xx): 
+        Q.append((yy, xx, cnt+1))
+        V[yy][xx] = 1
 print(-1)
--- baekjoon/18234.py
+++ baekjoon/18234.py
@@ -1,8 +1,5 @@
 # baekjoon 18234
-import sys
-input = sys.stdin.readline
-
 N, T = map(int, input().split())
-carrots, total = sorted([ list(map(int, input().split())) for _ in range(N) ], key=lambda x:-x[1]), 0
-for i in range(len(carrots)): total += carrots[i][0] + (T-i-1) * carrots[i][1]
-print(total)
+c, r = sorted([ list(map(int, input().split())) for _ in range(N) ], key=lambda x:-x[1]), 0
+for i in range(len(c)): r += c[i][0] + (T-i-1) * c[i][1]
+print(r)
