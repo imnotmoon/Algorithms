27688424252dd75958c73cf5901796b4052a2ae0
imnotmoon <imnotmoon@gmail.com>
2021-11-16 03:37:08 +0900

    9465: dp

--- baekjoon/9465.py
+++ baekjoon/9465.py
@@ -3,15 +3,14 @@ import sys
 input = sys.stdin.readline
 
 T = int(input())
-for t in range(T):
+for i in range(T):
     n = int(input())
-    sticker = [list(map(int, input().split())) for _ in range(2)]
-    dp = [[0 for _ in range(n)] for _ in range(2)]
-    dp[0][0], dp[1][0] = sticker[0][0], sticker[1][0]
-    dp[0][1], dp[1][1] = dp[1][0]+sticker[0][1], dp[0][0]+sticker[1][1]
+    numbers = [ list(map(int, input().split())) for _ in range(2) ]
+    dp = [ [0 for _ in range(n)] for _ in range(2) ]
+    dp[0][0], dp[1][0] = numbers[0][0], numbers[1][0]
 
-    for i in range(2, n):
-        dp[0][i] = max(dp[1][i-1], dp[0][i-2], dp[1][i-2]) + sticker[0][i]
-        dp[1][i] = max(dp[0][i-1], dp[0][i-2], dp[1][i-2]) + sticker[1][i]
-
+    for i in range(1, n):
+        for j in range(2):
+            dp[j][i] = max(dp[j][i], dp[1-j][i-1]) + numbers[j][i]
+            if i-2 >= 0 : dp[j][i] = max(dp[j][i], dp[1-j][i-2]+ numbers[j][i]) 
     print(max(dp[0][-1], dp[1][-1]))
