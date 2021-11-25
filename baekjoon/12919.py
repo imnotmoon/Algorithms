1c10ed1b52050dbf2bc4bd4462b5f37e620a3d89
imnotmoon <imnotmoon@gmail.com>
2021-11-26 02:19:19 +0900

    12919: refactor

--- baekjoon/12919.py
+++ baekjoon/12919.py
@@ -1,16 +1,5 @@
 # baekjoon 12919
-
-import sys
-input = sys.stdin.readline
-sys.setrecursionlimit(10**6)
-
-S, T = input().rstrip(), input().rstrip()
-
-def reverse(s):
-  t = ''
-  for i in s:
-    t = i + t
-  return t
+S, T = input(), input()
 
 def find(t):
   if len(t) < len(S): return
@@ -18,7 +7,7 @@ def find(t):
     print(1)
     exit(0)
   if t[-1] == 'A': find(t[:-1])
-  if t[0] == 'B': find(reverse(t[1:]))
+  if t[0] == 'B': find(t[1:][::-1])
 
 find(T)
 print(0)
