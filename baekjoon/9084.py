c27d9377aaa2d4825b5909cef2b5b4b441f80ac3
imnotmoon <imnotmoon@gmail.com>
2021-12-03 06:02:23 +0900

    *9084: knapsack

---
+++ baekjoon/9084.py
@@ -0,0 +1,17 @@
+# baekjoon 9084
+
+import sys
+input = sys.stdin.readline
+
+T = int(input())
+for t in range(T):
+  N = int(input())
+  costs = list(map(int, input().split()))
+  M = int(input())
+  dp = [ 1 if idx == 0 else 0 for idx in range(M+1) ]
+  for i in costs:
+    for j in range(1, M+1):
+      if j-i >= 0: dp[j] += dp[j-i]
+
+  print(dp[-1])
+
