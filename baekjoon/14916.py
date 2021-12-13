fa1c7749f90c1a5ccb60efa8d4a62fdc24639acd
imnotmoon <imnotmoon@gmail.com>
2021-12-14 04:36:02 +0900

    14916: dp

--- baekjoon/14916.py
+++ baekjoon/14916.py
@@ -1,19 +1,13 @@
 # baekjoon 14916
-n = int(input())
 
-dp = [1, -1, 1, -1, 2, 1]
-if n > 6:
-    for i in range(6, n+1):
-        minus_2 = dp[i-2]+1 if dp[i-2] > 0 else 0
-        minus_5 = 0
-        if i > 5:
-            minus_5 = dp[i-5]+1 if dp[i-5] > 0 else 0
-        if minus_2 > 0 and minus_5 > 0:
-            dp.append(min(minus_2, minus_5))
-        elif minus_5 == 0:
-            dp.append(minus_2)
-        elif minus_2 == 0:
-            dp.append(minus_5)
-        else:
-            dp.append(-1)
-print(dp[n])
+N = int(input())
+
+dp = [ float('inf') for _ in range(N+1) ]
+dp[0] = 0
+for i in range(2, N+1):
+    if i >= 5: dp[i] = min(dp[i-2], dp[i-5])+1
+    else: dp[i] = -1 if dp[i-2] < 0 else dp[i-2]+1
+
+print(dp)
+
+print(dp[-1] if dp[-1] != float('inf') else -1)
