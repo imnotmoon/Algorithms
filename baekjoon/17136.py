4b7f37eb7ec2fb5074c99aaac26af7ab05379919
imnotmoon <imnotmoon@gmail.com>
2021-12-25 21:38:04 +0900

    17136: edit

--- baekjoon/17136.py
+++ baekjoon/17136.py
@@ -3,14 +3,14 @@
 import sys
 sys.setrecursionlimit(10**6)
 input = sys.stdin.readline
-board = [ list(map(int, input().split())) for _ in range(10) ]
+paper = [ list(map(int, input().split())) for _ in range(10) ]
 remain = [5, 5, 5, 5, 5]
 total = 25
 
 def check(y, x, offset):
   for i in range(y, y+offset+1):  
     for j in range(x, x+offset+1):
-      if board[i][j] != 1: return False
+      if paper[i][j] != 1: return False
   return True
 
 def backtracking(y, x, c):
@@ -21,7 +21,7 @@ def backtracking(y, x, c):
   if x >= 10:
     backtracking(y+1, 0, c)
     return
-  if board[y][x] == 1:
+  if paper[y][x] == 1:
     for k in range(5):
       if remain[k] == 0: continue
       if y+k >= 10 or x+k >= 10: continue
@@ -29,17 +29,14 @@ def backtracking(y, x, c):
       if not check(y, x, k): break
       for i in range(y, y+k+1):
         for j in range(x, x+k+1):
-          board[i][j] = 0
+          paper[i][j] = 0
       remain[k] -= 1
       backtracking(y, x+k+1, c+1)
       remain[k] += 1
       for i in range(y, y+k+1):
         for j in range(x, x+k+1):
-          board[i][j] = 1
+          paper[i][j] = 1
   else : backtracking(y, x+1, c)
 
 backtracking(0, 0, 0)
-if total == 25:
-  print(-1)
-else :
-  print(total)
+print(-1 if total == 25 else total)
